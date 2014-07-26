99838be0ad98a54bacfb92dcb4bc7358e40e7805
MrD <the.ubik@gmail.com>
2014-07-26 17:12:59 +0100

    Initial commit

---
+++ gitter/git_api_wrapper.py
@@ -0,0 +1,51 @@
+import datetime
+
+__author__ = 'MrD'
+
+from  git import Repo, InvalidGitRepositoryError
+from  git import cmd, exc
+import os
+
+class Git(object):
+
+    def __init__(self, path, ignored_files=None):
+        print path
+        dir_ = os.path.abspath(path)
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+        elif not os.path.isdir(dir_):
+            raise RuntimeError(dir_ + "is not a directory")
+        try:  # http://stackoverflow.com/a/23666860/281545
+            self.repo = Repo(dir_)
+            self._g = _g = cmd.Git(dir_)
+            try:
+                _g.add('-A')
+                _g.commit(str(datetime.date.today))
+            except exc.GitCommandError:
+                # see: http://stackoverflow.com/a/21078070/281545
+                pass
+        except InvalidGitRepositoryError:
+            print 'isn`t git repo'
+            self._g = _g = cmd.Git(dir_)
+            _g.init()
+            self.repo = repo = Repo()
+            if ignored_files:
+                with open(os.path.join(repo.git_dir,"/info/exclude"),
+                          'w') as exclude:
+                    exclude.truncate()
+                    for path in ignored_files:
+                        exclude.write(str(path) + '\n')
+            try:
+                _g.add('-A')
+                _g.commit("Initial commit")
+            except exc.GitCommandError:
+                pass
+
+    def commit(self, message):
+        pass
+
+    def getIgnoredPaths(self, message):
+        from  git import cmd
+    def dir(self):
+        repo_git_dir = self.repo.git_dir
+        return repo_git_dir
---
+++ watcher/watchdog_demo.py
@@ -0,0 +1,58 @@
+__author__ = 'https://github.com/gorakhargosh/watchdog'
+import sys
+import time
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+from gitter import git_api_wrapper
+
+class TestEventHandler(PatternMatchingEventHandler):
+    """Logs all the events captured."""
+
+    def __init__(self, ignore_patterns=("*.git*",)):
+        """
+
+        :param ignore_patterns: a list/tuple of wirldcard patterns - see
+        Python27\Lib\site-packages\pathtools\patterns.py.match_path
+        """
+        super(TestEventHandler, self).__init__(ignore_patterns=ignore_patterns)
+
+    def on_moved(self, event):
+        super(TestEventHandler, self).on_moved(event)
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Moved %s: from %s to %s", what, event.src_path,
+                     event.dest_path)
+
+    def on_created(self, event):
+        super(TestEventHandler, self).on_created(event)
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Created %s: %s", what, event.src_path)
+
+    def on_deleted(self, event):
+        super(TestEventHandler, self).on_deleted(event)
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Deleted %s: %s", what, event.src_path)
+
+    def on_modified(self, event):
+        super(TestEventHandler, self).on_modified(event)
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Modified %s: %s", what, event.src_path)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '../../sandbox'
+    git = git_api_wrapper.Git(path)
+    event_handler = TestEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
