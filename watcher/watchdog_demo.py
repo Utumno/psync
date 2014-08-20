a1eeb7f4ce8dc317fcc5ad76eabda7bd97a18b98
MrD <the.ubik@gmail.com>
2014-08-20 21:07:08 +0300

    event handler moved to module

--- watcher/watchdog_demo.py
+++ watcher/watchdog_demo.py
@@ -1,42 +1,11 @@
 import logging
 import shlex
 from watchdog.observers import Observer
-from watchdog.events import RegexMatchingEventHandler
+
 # internal imports
 from watcher.cli import Parser
 from gitter import git_api_wrapper
-
-class TestEventHandler(RegexMatchingEventHandler):
-    """Logs all the events captured."""
-
-    def __init__(self, ignore_regexes=(".*\.git.*",)):
-        """
-
-        :param ignore_patterns: a list/tuple of wirldcard patterns - see
-        Python27\Lib\site-packages\pathtools\patterns.py.match_path
-        """
-        super(TestEventHandler, self).__init__(ignore_regexes=ignore_regexes)
-
-    def on_moved(self, event):
-        super(TestEventHandler, self).on_moved(event)
-        what = 'directory' if event.is_directory else 'file'
-        logging.info("Moved %s: from %s to %s", what, event.src_path,
-                     event.dest_path)
-
-    def on_created(self, event):
-        super(TestEventHandler, self).on_created(event)
-        what = 'directory' if event.is_directory else 'file'
-        logging.info("Created %s: %s", what, event.src_path)
-
-    def on_deleted(self, event):
-        super(TestEventHandler, self).on_deleted(event)
-        what = 'directory' if event.is_directory else 'file'
-        logging.info("Deleted %s: %s", what, event.src_path)
-
-    def on_modified(self, event):
-        super(TestEventHandler, self).on_modified(event)
-        what = 'directory' if event.is_directory else 'file'
-        logging.info("Modified %s: %s", what, event.src_path)
+from watcher.event_handler import TestEventHandler
 
 class Sync():
 
