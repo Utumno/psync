4b28de684d4b60065c656a3ff903f489ca4c159b
kleom <kleomenis.tsiligkos@gmail.com>
2014-09-01 21:14:47 +0300

    The server that is responsible for communicating the unknown address of the server to the broadcasting client.

---
+++ server/DiscoveryServer.py
@@ -0,0 +1,30 @@
+import SocketServer
+import socket               # Import socket module
+
+class DiscoveryServer(object):
+
+    host =  socket.gethostbyname(socket.gethostname())
+
+    class DiscoveryUDPHandler(SocketServer.BaseRequestHandler):
+        ''' Here we implement the required service '''
+
+        def handle(self):
+            data = self.request[0].strip()
+            print "self.request value is ", self.request[0]
+            socket = self.request[1]
+            print 'Got that: ', data
+            socket.sendto(str(DiscoveryServer.host), self.client_address)
+
+    def __init__(self):
+            self.port = 8001                # Reserve a port for your service.
+            print self.host
+            print self.port
+
+    def activate(self):
+        server = SocketServer.UDPServer((self.host, self.port), DiscoveryServer.DiscoveryUDPHandler)
+        server.serve_forever()
+
+if __name__ == "__main__":
+     x = DiscoveryServer()
+     x.activate()
+
