298a199b94b9e69ef62024081b9309fa11b49b28
kleom <kleomenis.tsiligkos@gmail.com>
2014-09-02 20:43:33 +0300

    SocketClient: base class for all clients Added: TransferClient Changes to DiscoveryClient so as to inherit the SocketClient

---
+++ server/SocketClient.py
@@ -0,0 +1,36 @@
+import socket
+
+DISCOVERY_MODE = 0
+TRANSFER_MODE = 1
+
+class BaseClient(object):
+    def __init__(self, socket_type = 0, host = "0.0.0.0", port = 0):
+        self.setup(socket_type, host, port)
+        self.service()
+        self.close_connection()
+
+    def setup(self, socket_type, host, port):
+        if socket_type is 0:
+            try:
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            except socket.error as e:
+                print "Failed to create the socket. ERROR Code: ", e.errno, " Message: ",e.message
+        else:
+             try:
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s.connect((host, port))
+             except socket.error as e:
+                print "Failed to create the socket. ERROR Code: ", e.errno, " Message: ",e.message
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.settimeout(1) #FIXME
+
+    def service(self):
+        """
+        Called by the constructor
+        Must be overridden
+        """
+        pass
+
+    def close_connection(self):
+        self.s.close()
--- server/TransferClient.py
+++ server/TransferClient.py
@@ -1,21 +1,24 @@
 import SocketClient
 import socket
 
-class DiscoveryClient(SocketClient.BaseClient):
+class TransferClient(SocketClient.BaseClient):
+
+    HOST = "192.168.1.9"
+    PORT = 8000
+
     def __init__(self):
-      SocketClient.BaseClient.__init__(self,SocketClient.DISCOVERY_MODE)
+      SocketClient.BaseClient.__init__(self,SocketClient.TRANSFER_MODE, TransferClient.HOST, TransferClient.PORT)
 
 
     def service(self):
         ''' This method discovers if there is an active sync application in the LAN '''
-        self.s.sendto("DISCOVERY MESSAGE", ('255.255.255.255', 8001))
+        self.s.sendall("ACTUAL GIT OPERATIONS")
         try:
             c, addr =  self.s.recvfrom(1024)
             print "The received message's payload is ",c
-            print "The responding server address is ",addr
         except socket.timeout as e:
             msg  = e.message
             print msg.upper()
 
 if __name__ == "__main__":
-    DiscoveryClient()
+   TransferClient()
