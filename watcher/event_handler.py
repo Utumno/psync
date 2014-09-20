cf93b143eda31eedd2498adf59e439db0e88baf1
MrD <the.ubik@gmail.com>
2014-09-20 03:41:44 +0100

    Fix for commit messages

--- watcher/event_handler.py
+++ watcher/event_handler.py
@@ -18,23 +18,24 @@ class TestEventHandler(RegexMatchingEventHandler):
         what = 'directory' if event.is_directory else 'file'
         logging.info("Moved %s: from %s to %s", what, event.src_path,
                      event.dest_path)
-        self.gitter.commitAll(msg=("Moved %s: from %s to %s", what, event.src_path,
-                     event.dest_path))
+        self.gitter.commitAll(msg=(
+        "Moved %s: from %s to %s" % (what, event.src_path, event.dest_path)))
 
     def on_created(self, event):
         super(TestEventHandler, self).on_created(event)
         what = 'directory' if event.is_directory else 'file'
         logging.info("Created %s: %s", what, event.src_path)
-        self.gitter.commitAll(msg=("Created %s: %s", what, event.src_path))
+        self.gitter.commitAll(msg=("Created %s: %s" % (what, event.src_path)))
 
     def on_deleted(self, event):
         super(TestEventHandler, self).on_deleted(event)
         what = 'directory' if event.is_directory else 'file'
         logging.info("Deleted %s: %s", what, event.src_path)
-        self.gitter.commitAll(msg=("Deleted %s: %s", what, event.src_path))
+        self.gitter.commitAll(msg=("Deleted %s: %s" % ( what, event.src_path)))
 
     def on_modified(self, event):
         super(TestEventHandler, self).on_modified(event)
         what = 'directory' if event.is_directory else 'file'
         logging.info("Modified %s: %s", what, event.src_path)
-        self.gitter.commitAll(msg=("Modified %s: %s", what, event.src_path))
+        self.gitter.commitAll(
+            msg=("Modified %s: %s" % ( what, event.src_path)))
